a5a238b2ef7f6282958751b7dbe56f0a7e038e11
Ezeger <52760933+Ezeger@users.noreply.github.com>
2019-07-11 15:21:51 -0500

    Add files via upload

---
+++ myScript.js
@@ -0,0 +1,5 @@
+window.alert("Testing! You may ignore.");
+
+/*  What alse do I want to do? Break up the <p> text more.
+*/
+// Pop up text? Add in something new if clicked? Reward exploration though theres nothing to explore???
